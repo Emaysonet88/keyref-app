0bb908425469c07ee2bbd5f3c59d4a077cc39fc9
Efrain Maysonet <Emaysonet88@gmail.com>
2026-05-13 21:10:33 -0400

    Session 1: cleanup, data quality fixes, OBP procedures, new favicon/title

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import KeyRefPro from './KeyRefProWIP';
-import Settings from './Settings';
-import './App.css';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<KeyRefPro />} />
-        <Route path="/settings" element={<Settings />} />
       </Routes>
     </Router>
   );
